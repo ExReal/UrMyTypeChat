9eec1d2a8520b1bbaf3a9c860ecd03a4923803f0
Minamimoto97 <37876643+ExReal@users.noreply.github.com>
2022-10-11 22:14:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ var callback = data => {
     var results = "";
     results += "<center> <h1>Ur My Type <br> Chat History</h1> </center>"
     app.mensajes.map(mensaje => {
-        if (mensaje.name == "Madeleine") {
-            results += "<div id='message-madeleine'>";
-            results += "<div id='mensajero'>" + mensaje.name + "</div>"
-            results += "<div id='text-madeleine'>" + mensaje.text + "</div>";   
-            results += "</div>";
-        }
         if (mensaje.name == "Miguel") {
             results += "<div id='message-miguel'>";
             results += "<div id='mensajero'>" + mensaje.name + "</div>"
-            results += "<div id='text-miguel'>" + mensaje.text + "</div>";   
+            results += "<div id='text-miguel'>" + mensaje.text + "</div> <br>";
+            results += "<div id='time-1'>" + mensaje.time + "</div>";   
+            results += "</div>";
+        }
+        if (mensaje.name == "Madeleine") {
+            results += "<div id='message-madeleine'>";
+            results += "<div id='mensajero'>" + mensaje.name + "</div>"
+            results += "<div id='text-madeleine'>" + mensaje.text + "</div> <br>";
+            results += "<div id='time-2'>" + mensaje.time + "</div>";   
             results += "</div>";
         }
     })
